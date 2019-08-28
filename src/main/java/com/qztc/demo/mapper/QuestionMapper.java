f588a7c155acc64f0e7334260bfb9ae0a6a93a1a
yechengchao <yechengchao9707@163.com>
2019-08-28 09:49:55 +0800

    添加了学生疑问表，数据库的sql记得更新

---
+++ src/main/java/com/qztc/demo/mapper/QuestionMapper.java
@@ -0,0 +1,17 @@
+package com.qztc.demo.mapper;
+
+import com.qztc.demo.model.Question;
+
+public interface QuestionMapper {
+    int deleteByPrimaryKey(Integer questionId);
+
+    int insert(Question record);
+
+    int insertSelective(Question record);
+
+    Question selectByPrimaryKey(Integer questionId);
+
+    int updateByPrimaryKeySelective(Question record);
+
+    int updateByPrimaryKey(Question record);
+}
---
+++ src/main/java/com/qztc/demo/model/Question.java
@@ -0,0 +1,63 @@
+package com.qztc.demo.model;
+
+public class Question {
+    private Integer questionId;
+
+    private Integer studentId;
+
+    private Integer courseId;
+
+    private Integer workId;
+
+    private Integer teacherId;
+
+    private String questionContent;
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public Integer getWorkId() {
+        return workId;
+    }
+
+    public void setWorkId(Integer workId) {
+        this.workId = workId;
+    }
+
+    public Integer getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Integer teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent == null ? null : questionContent.trim();
+    }
+}
